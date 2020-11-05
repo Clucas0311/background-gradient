48c240b923235e67757f546f0bf9174db7bf29ee
Clucas0311 <clucas0311@gmail.com>
2020-11-05 18:01:46 -0500

    Added ID

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const color2 = document.querySelector('.color2');
 color1.addEventListener('input', function() {
 	console.log(color1.value);
 });
+
 color2.addEventListener('input', function() {
 	console.log(color2.value);
 });
