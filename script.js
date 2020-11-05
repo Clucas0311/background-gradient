92f5de2c899898d2376890cb0610b5c15376f3f4
Clucas0311 <clucas0311@gmail.com>
2020-11-05 17:59:50 -0500

    Add script tag

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h3 = document.querySelector('h3');
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector('.color2');
+
+color1.addEventListener('input', function() {
+	console.log(color1.value);
+});
+color2.addEventListener('input', function() {
+	console.log(color2.value);
+});
