1cf4f54b2de1decfccae27400853b203088b14fc
Clucas0311 <clucas0311@gmail.com>
2020-11-05 18:19:20 -0500

    clean code

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const h3 = document.querySelector('h3');
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector('.color2');
+const body = document.querySelector('#gradient');
 
-color1.addEventListener('input', function() {
-	console.log(color1.value);
-});
+function setGradient() {
+	body.style.background = 'linear-gradient(to right, ' + color1.value + ',' + color2.value + ')';
+}
+color1.addEventListener('input', setGradient);
 
-color2.addEventListener('input', function() {
-	console.log(color2.value);
-});
+color2.addEventListener('input', setGradient);
